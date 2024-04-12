8af0228fa1a4ce47955ec69f36f8a64770ee51d3
My Name <fesz99@naver.com>
2024-04-12 18:09:22 +0900

    js study

---
+++ p04/JS0412/js/main.js
@@ -0,0 +1,168 @@
+$(function(){
+  //최초 실행되는 부분
+    let no = [1,2,3,4,5,6,7,8,9,10];
+    let name = ['홍길동','유관순','이순신','김구','강감찬','김유신','홍길순','홍길자','최현묵','이규원'];
+    let kor = [62,90,64,76,51,89,77,55,73,53];
+    let eng = [70,62,73,54,55,60,67,77,51,100];
+    let math = [81,79,50,83,72,79,82,86,50,94];
+    let total = [213,231,187,213,178,228,226,218,174,247];
+    let avg = [71,77,62.3,71,59.3,76,75.3,72.7,58,82.3];
+    
+    //tbody부문 10개 행 생성
+    let htmlData = "";
+    for(i=0; i<no.length; i++){
+        htmlData += "<tr id='"+no[i]+"'>";
+        htmlData += "<td><input type='checkbox' name='stu' class='stuChk' value='"+no[i]+"'></td>";
+        htmlData += "<td>"+no[i]+"</td>";
+        htmlData += "<td>"+name[i]+"</td>";
+        htmlData += "<td>"+kor[i]+"</td>";
+        htmlData += "<td>"+eng[i]+"</td>";
+        htmlData += "<td>"+math[i]+"</td>";
+        htmlData += "<td>"+total[i]+"</td>";
+        htmlData += "<td>"+avg[i]+"</td>";
+        htmlData += "<td>0</td>";
+        htmlData += "<td><button class='delBtn'>삭제</button></td>";
+        htmlData += "</tr>";     
+    }//for
+    
+    //html소스를 tbody에 추가
+    $("#body").html(htmlData);
+
+  //최초 실행되는 부분------------------------------
+
+    // 학생성적 입력창이 팝업으로 뜨도록
+    $("#writeBtn").click(function(){
+        alert("writebtn test")
+        $(".p_all").css("display","block");
+    });//학생성적 입력창
+    
+    //학생성적 입력창 닫기
+    $("#cancelBtn").click(function(){
+        alert("writebtn test")
+        $(".p_all").css("display","none");
+    });//학생성적 입력창 닫기
+
+    //학생입력확인버튼
+    $("#confirmBtn").click(function(){
+        //글자text(), input val(), html()
+        //console.log("이름: "+$("#name").val());
+        //console.log("국어: "+$("#kor").val());
+        //console.log("영어: "+$("#eng").val());
+        //console.log("수학: "+$("#math").val());
+        //console.log(Math.max.apply(null,no)+1); //this, 여기에 들어갈 값이 없기에 null표시
+        //no.push(Math.max.apply(null,no)+1); //배열추가
+
+        //공백확인, input창에 입력이 안되어있으면 alert이 뜬다
+        if($("#name").val().length<2){ //글씨가 두글자 이하면,
+            alert("이름을 입력하시오 (2글자 이상)")
+            $("#name").focus();
+            return false;
+        }
+
+        //번호생성은 DB에서 자동으로 부여됨,
+        let i_no = Math.max.apply(null,no)+1
+        no.push(i_no);
+        let i_name = $("#name").val();
+        let i_kor = Number($("#kor").val());
+        let i_eng = Number($("#eng").val());
+        let i_math = Number($("#math").val());
+        let i_total = i_kor+i_eng+i_math;
+        let i_avg =(i_total/3).toFixed(2); //소수점 2자리 반올림
+        let i_rank = 0;
+
+        // table tr추가하기(학생입력)
+        let htmlData = "";
+        htmlData += "<tr id='"+i_no+"'>";
+        htmlData += "<td><input type='checkbox' name='stu' class='stuChk' value='"+i_no+"'></td>";
+        htmlData += "<td>"+i_no+"</td>";
+        htmlData += "<td>"+i_name+"</td>";
+        htmlData += "<td>"+i_kor+"</td>";
+        htmlData += "<td>"+i_eng+"</td>";
+        htmlData += "<td>"+i_math+"</td>";
+        htmlData += "<td>"+i_total+"</td>";
+        htmlData += "<td>"+i_avg+"</td>";
+        htmlData += "<td>"+i_rank+"</td>";
+        htmlData += "<td><button class='delBtn'>삭제</button></td>";
+        htmlData += "</tr>";     
+    
+    //html소스를 tbody에 추가, 기존 html을 덮어쓰기
+    //$("#body").html(htmlData);
+    //$("#body").parent(htmlData); //기존 html 위쪽에 추가
+    $("#body").append(htmlData); //기존 html 뒤쪽에 추가
+
+    //input초기화, 빈공백을 입력해라
+    $("#name").val("");
+    $("#kor").val("");
+    $("#eng").val("");
+    $("#math").val("");
+    $(".p_all").css("display","none");
+    
+})//학생입력버튼
+
+    //전체선택, 취소
+    $("#allChk").click(function(){
+        if($(this).is(":checked")==true){// 좌상단 체크박스 클릭시
+            //모두 체크
+            console.log("체크됨")
+            //each, 배열만큼 반복
+            $(".stuChk").each(function(){
+                $(this).prop("checked",true); //.stuChk의 each만큼 체크를 true(on) 
+            })
+        }else{
+            //모두 체크 해제
+            console.log("체크해제")
+            $(".stuChk").each(function(){
+                $(this).prop("checked",false); //.stuChk의 each만큼 체크를 false(off) 
+            })
+        }//else
+
+    });//좌상단 체크박스 클릭시
+
+    //table에 있는 삭제버튼, document에서 .delBtn을 다시 찾아야한다. 추가등록이 삭제 안되는 경우;;
+    $(document).on("click",".delBtn",function(){
+        console.log($(this).parent().parent().attr("id"));
+        if(confirm("정말 삭제하시겠습니까?")){
+            $("#"+$(this).parent().parent().attr("id")).remove();
+        }
+    });
+
+    /**
+    $(".delBtn").click(function(){
+        console.log("1")
+        console.log("현재 선택된id: "+$(this).parent().parent().attr("id"));
+        if(confirm("삭제하시겠습니까?")){
+            $("#"+$(this).parent().parent().attr("id")).remove();
+        }
+    });//table 삭제 */
+    
+    //하단 삭제버튼 클릭
+    $("#deletBtn").click(function(){
+        //alert("test");
+        console.log("체크박스 갯수: "+$(".stuChk").length);
+        console.log("체크된 박스 갯수: "+$(".stuChk:checked").length);
+        //console.log("체크된 박스 갯수: "+$("input:checkbox[name='stu']:checked").length);
+        
+        //체크되어 있는 것이 없을 경우
+        if($(".stuChk:checked").length<1){
+            alert("삭제할 데이터를 체크하세요");
+            return false;
+        }
+        //현재 박스가 체크되어있는지 확인
+        if(!confirm("정말 삭제하시겠습니까?")){
+            return false; //no버튼 클릭시 다시 돌아감, yes누를 시 패스
+        }
+        //모든 체크박스를 가져오기
+        $(".stuChk").each(function(){
+
+
+            if($(this).is(":checked") ==true){
+                console.log("현재 선택된class: "+$(this).val());
+                console.log("현재 선택된idd: "+$(this).parent().parent().attr("id"));
+                $("#"+$(this).parent().parent().attr("id")).remove();
+            }
+        });//each
+    });//하단삭제버튼
+    
+});//제이쿼리
+    
+    
--- p04/p0407/p0407_01.py
+++ p04/p0407/p0407_01.py
@@ -1,107 +1,133 @@
 import pygame
 import random
+import time
+from datetime import datetime
 
-# 1. pygame 초기화
+# 1. 게임 초기화
 pygame.init()
 
-# 2. 게임창 설정
-size = [400, 900]  # 게임 화면 크기 설정
-screen = pygame.display.set_mode(size)  # 화면 생성
+# 2. 게임창 옵션 설정
+size = [400, 900]
+screen = pygame.display.set_mode(size)
 
-title = "My game"
-pygame.display.set_caption(title)  # 게임 창 타이틀 설정
+title = "My Game"
+pygame.display.set_caption(title)
 
 # 3. 게임 내 필요한 설정
-clock = pygame.time.Clock()  # 게임 시간 관리를 위한 Clock 객체 생성
+clock = pygame.time.Clock()
 
 class obj:
     def __init__(self):
-        self.x = 0  # 객체의 x 좌표
-        self.y = 0  # 객체의 y 좌표
-        
+        self.x = 0
+        self.y = 0
+        self.move = 0
     def put_img(self, address):
         if address[-3:] == "png":
-            self.img = pygame.image.load(address).convert_alpha()  # PNG 이미지를 불러와서 알파 채널을 포함하여 이미지 로드
-        else:
-            self.img = pygame.image.load(address)  # PNG가 아닌 경우 일반적으로 이미지 로드
-        self.sx, self.sy = self.img.get_size()  # 이미지 크기 저장
-        
+            self.img = pygame.image.load(address).convert_alpha()
+        else :
+            self.img = pygame.image.load(address)
+        self.sx, self.sy = self.img.get_size()
     def change_size(self, sx, sy):
-        self.img = pygame.transform.scale(self.img, (sx, sy))  # 이미지 크기 조정
-        self.sx, self.sy = self.img.get_size()  # 이미지 크기 갱신
-        
+        self.img = pygame.transform.scale(self.img, (sx, sy))
+        self.sx, self.sy = self.img.get_size()
     def show(self):
-        screen.blit(self.img, (self.x, self.y))  # 화면에 이미지 그리기
+        screen.blit(self.img, (self.x,self.y))
         
-# 우주선 객체 생성 및 설정
-ss = obj()        
-ss.put_img("ss.png")  # 우주선 이미지 불러오기
-ss.change_size(50, 80)  # 이미지 크기 조정
-ss.x = round(size[0] / 2 - ss.sx / 2)  # 화면 가로 중앙에 우주선 위치
-ss.y = size[1] - ss.sy - 15  # 화면 하단에 우주선 위치
-ss.move = 5  # 우주선 이동 속도
+def crash(a, b):
+    if (a.x-b.sx <= b.x) and (b.x <= a.x+a.sx):
+        if (a.y-b.sy <= b.y) and (b.y <= a.y+a.sy):
+            return True
+        else:
+            return False
+    else : 
+        return False
+ss = obj()
+ss.put_img("ss.png")
+ss.change_size(50,80)
+ss.x = round(size[0]/2- ss.sx/2)
+ss.y = size[1] -ss.sy - 15
+ss.move = 5
 
-# 키보드 입력 관련 변수 초기화
 left_go = False
 right_go = False
 space_go = False
 
-# 미사일과 적 우주선 목록 초기화
-m_list = []  # 미사일 목록
-a_list = []  # 적 우주선 목록
+m_list = []
+a_list = []
 
-# 색상 정의
-black = (0, 0, 0)
+black = (0,0,0)
+white = (255,255,255)
+k = 0
 
-# 게임 루프
-running = True
-while running:
-    # 4-1. FPS 설정
-    clock.tick(60)  # 초당 60프레임으로 설정
+GO = 0
+kill = 0
+loss = 0
+
+# 4-0. 게임 시작 대기 화면
+SB = 0
+while SB == 0:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                SB = 1
+    screen.fill(black)
     
-    # 4-2. 이벤트 처리
+    
+    
+    pygame.display.flip()
+
+# 4. 메인 이벤트
+start_time = datetime.now()
+SB = 0
+while SB == 0:
+
+    # 4-1. FPS 설정
+    clock.tick(60)
+
+    # 4-2. 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False  # 종료 버튼을 누르면 게임 루프 종료
-        
+            SB = 1
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                left_go = True  # 왼쪽 화살표 키 누름
+                left_go = True
             elif event.key == pygame.K_RIGHT:
-                right_go = True  # 오른쪽 화살표 키 누름
+                right_go = True
             elif event.key == pygame.K_SPACE:
-                space_go = True  # 스페이스바 키 누름
-                
+                space_go = True
+                k = 0
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                left_go = False  # 왼쪽 화살표 키 뗌
+                left_go = False
             elif event.key == pygame.K_RIGHT:
-                right_go = False  # 오른쪽 화살표 키 뗌
+                right_go = False
             elif event.key == pygame.K_SPACE:
-                space_go = False  # 스페이스바 키 뗌
+                space_go = False 
+                
+    # 4-3. 입력, 시간에 따른 변화
+    now_time = datetime.now()
+    delta_time = round((now_time - start_time).total_seconds())
     
-    # 4-3. 입력 및 게임 로직 처리
-    if left_go:
-        ss.x -= ss.move  # 왼쪽 이동
+    if left_go == True:
+        ss.x -= ss.move
         if ss.x <= 0:
             ss.x = 0
-    elif right_go:
-        ss.x += ss.move  # 오른쪽 이동
+    elif right_go == True:
+        ss.x += ss.move
         if ss.x >= size[0] - ss.sx:
             ss.x = size[0] - ss.sx
     
-    # 미사일 발사 로직
-    if space_go:
+    if space_go == True and k % 6 == 0:
         mm = obj()
-        mm.put_img("yellow.png")  # 미사일 이미지 불러오기
-        mm.change_size(5, 15)  # 미사일 크기 조정
-        mm.x = round(ss.x + ss.sx / 2 - mm.sx / 2)  # 우주선 중앙에서 발사
+        mm.put_img("mm.png")
+        mm.change_size(5,15)
+        mm.x = round(ss.x + ss.sx/2 - mm.sx/2)
         mm.y = ss.y - mm.sy - 10
-        mm.move = 15  # 미사일 이동 속도
+        mm.move = 15
         m_list.append(mm)
-    
-    # 미사일 이동 처리
-    d_list = []
+    k += 1
+    d_list = []    
     for i in range(len(m_list)):
         m = m_list[i]
         m.y -= m.move
@@ -111,17 +137,14 @@ while running:
     for d in d_list:
         del m_list[d]
         
-    # 적 우주선 생성
-    if random.random() > 0.98:
+    if random.random() > 0.98: 
         aa = obj()
-        aa.put_img("aa.png")  # 적 우주선 이미지 불러오기
-        aa.change_size(40, 40)  # 적 우주선 크기 조정
-        aa.x = random.randrange(0, size[0] - aa.sx - round(ss.sx / 2))  # 화면 내 임의 위치에서 생성
-        aa.y = 10  # 화면 상단에서 생성
-        aa.move = 1  # 적 우주선 이동 속도
+        aa.put_img("aa.png")
+        aa.change_size(40,40)
+        aa.x = random.randrange(0, size[0]-aa.sx-round(ss.sx/2))
+        aa.y = 10
+        aa.move = 1
         a_list.append(aa)
-    
-    # 적 우주선 이동 처리
     d_list = []
     for i in range(len(a_list)):
         a = a_list[i]
@@ -131,6 +154,57 @@ while running:
     d_list.reverse()
     for d in d_list:
         del a_list[d]
+        loss += 1
+    
+    dm_list = []
+    da_list = []
+    for i in range(len(m_list)):
+        for j in range(len(a_list)):
+            m = m_list[i]
+            a = a_list[j]
+            if crash(m,a) == True:
+                dm_list.append(i)
+                da_list.append(j)
+    dm_list = list(set(dm_list))
+    da_list = list(set(da_list))
+    dm_list.reverse()
+    da_list.reverse()
+    try:
+        for dm in dm_list:
+            del m_list[dm]
+        for da in da_list:
+            del a_list[da]
+            kill += 1
+    except:
+        pass
         
-    # 4-4. 화면 그리기
-   
+    for i in range(len(a_list)):
+        a = a_list[i]
+        if crash(a, ss) == True:
+            SB = 1
+            GO = 1
+    
+    # 4-4. 그리기
+    screen.fill(black)
+    ss.show()
+    for m in m_list:
+        m.show()
+    for a in a_list:
+        a.show()
+        
+    
+    
+    # 4-5. 업데이트
+    pygame.display.flip()
+
+# 5. 게임 종료
+while GO == 1:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GO = 0
+    font = pygame.font.Font("C:/Windows/Fonts/BMDOHYEON_ttf.ttf", 40)
+    text = font.render("GAME OVER", True, (255,0,0))
+    screen.blit(text, (80, round(size[1]/2-50)))
+    pygame.display.flip()
+pygame.quit()
